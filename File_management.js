aa05ebda5c223dba22f63e670e9cd2a77321d4dd
Brian <58400498+nairb725@users.noreply.github.com>
2021-01-27 14:57:18 +0100

    Update File_management.js
    in progress

--- File_management.js
+++ File_management.js
@@ -1,6 +1,20 @@
-fs=require('fs')
+let json = ({"id":"287947","title":"Shazam! ","poster":"https://image.tmdb.org/t/p/w500/xnopI5Xtky18MPhK40cZAGAOVeV.jpg","overview":"A boy is given the ability to become an adult superhero in times of need with a single magic word.","release_date":1553299200,"genres":["Action","Comedy","Fantasy"]});
+console.log(json);
 
-fs.readFile('movies.json', (err, data) => {
-    if (err) throw err;
-    console.log(JSON.parse(data));
-  });
+
+let date = new Date(json.release_date * 1000)
+console.log(date);
+
+let year = date.getYear()+1900
+console.log(year);
+
+
+
+
+
+
+console.log({id : json.id , title : json.title + year, poster : json.poster ,overview : json.overview ,release_date : json.release_date ,genres : json.genres});
+
+
+
+console.log(process.argv)
