e03f1c26a5545ee4e0d9f1be2f67d192f4d9eeac
Yanis <yanisjdz@outlook.fr>
2021-01-27 13:13:04 +0100

    add sort_title (+case)

--- index.js
+++ index.js
@@ -18,12 +18,21 @@ if (myArgs[0] === '-action'){
       sort_date(input_dir,output_dir);
       break;
 
+    case 'sort_title': 
+    console.log('Tri par titre');
+    //Stockage des input/output (strings) 
+    input_dir = myArgs[2];
+    output_dir = myArgs[3];
+    console.log("Fichier d'input: " + input_dir);
+    console.log("Dossier d'output: " + output_dir);
+    //Lancer fonction tri titre
+    sort_title(input_dir,output_dir);
+    break;
     //Dans le cas où il y a une erreur d'argument  
     default:
       console.log("Je n'ai pas compris..")
   }
 }
-
 
 //Fonction lecture du fichier + écriture: film triés par date croissante
 function sort_date(input,output_dir){
@@ -47,13 +56,13 @@ function tri_date(tab,first,last){
   //Si premier < dernier
   if (first<last){
     let pivot = Math.floor((first+last)/2)
-    pivot = partitionner(tab,first,last,pivot)
+    pivot = part_date(tab,first,last,pivot)
     tri_date(tab,first,pivot-1)
     tri_date(tab,pivot+1,last)
   }
 }
 
-function partitionner(tab,first,last,pivot){
+function part_date(tab,first,last,pivot){
   //Echanger tab[pivot] avec tab[last]
   swap(tab,pivot,last)
   //j premier élément
@@ -62,6 +71,54 @@ function partitionner(tab,first,last,pivot){
   for (i=first;i<=last-1;i++){
     //Tri croissant ici
     if (tab[i].release_date <tab[last].release_date){
+      //Echange tab[i] et tab[last] pour respecter l'ordre croissant
+      swap(tab,i,j);
+      j=j+1;
+    }
+  }
+  //Pour échanger tab[last] et tab[j]
+  swap(tab,last,j)
+  //Renvoie j
+  return j
+}
+
+//Fonction lecture du fichier + écriture: film triés par ordre alphabétique
+function sort_title(input,output_dir){
+  //Lecture du fichier 'input'
+  fs.readFile(input, (err, data) => {
+    if (err) throw err;
+    //Stock des données dans tab
+    let tab = (JSON.parse(data));
+    //Tri des données
+    tri_title(tab,0,tab.length-1)
+    //Ecriture du fichier 'output' avec les films trié par ordre alphabétique
+    fs.writeFile(output_dir,JSON.stringify(tab,null,'\t'),function(err) {
+      if(err) return console.error(err);
+      console.log('Fichier trié par ordre alphabétique (titres) créé.');
+      })
+  });
+}
+
+//Fonction tri titre de films par ordre alphabétique
+function tri_title(tab,first,last){
+  //Si premier < dernier
+  if (first<last){
+    let pivot = Math.floor((first+last)/2)
+    pivot = part_title(tab,first,last,pivot)
+    tri_title(tab,first,pivot-1)
+    tri_title(tab,pivot+1,last)
+  }
+}
+
+function part_title(tab,first,last,pivot){
+  //Echanger tab[pivot] avec tab[last]
+  swap(tab,pivot,last)
+  //j premier élément
+  j = first;
+  //Exploration du tableau
+  for (i=first;i<=last-1;i++){
+    //Tri croissant ici
+    if (tab[i].title <tab[last].title){
       //Echange tab[i] et tab[last] pour respecter l'ordre croissant
       swap(tab,i,j);
       j=j+1;
