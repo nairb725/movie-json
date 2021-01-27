d2443636d58c42757f16f8298c47a3892076291b
Brian <58400498+nairb725@users.noreply.github.com>
2021-01-27 15:13:09 +0100

    Update File_management.js
    commentary

--- File_management.js
+++ File_management.js
@@ -1,20 +1,9 @@
 let json = ({"id":"287947","title":"Shazam! ","poster":"https://image.tmdb.org/t/p/w500/xnopI5Xtky18MPhK40cZAGAOVeV.jpg","overview":"A boy is given the ability to become an adult superhero in times of need with a single magic word.","release_date":1553299200,"genres":["Action","Comedy","Fantasy"]});
-console.log(json);
-
 
 let date = new Date(json.release_date * 1000)
-console.log(date);
-
+//mise des millisecondes en secondes et 
 let year = date.getYear()+1900
-console.log(year);
-
-
-
-
-
+//création d'une date
 
 console.log({id : json.id , title : json.title + year, poster : json.poster ,overview : json.overview ,release_date : json.release_date ,genres : json.genres});
-
-
-
-console.log(process.argv)
+//remise en forme json
